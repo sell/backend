3319e9fbd2241e5d176a2c4fe9e192bf1e1f2092
Jay <34258275+sell@users.noreply.github.com>
2020-07-03 23:54:52 -0400

    added cors

--- src/server.js
+++ src/server.js
@@ -18,13 +18,9 @@ mongoose.connect(db, {useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-app.use('/api/inputs', items)
+app.use('/api/inputs', items, cors())
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true);
     res.status(404).send('not found buddy')
 })
 
