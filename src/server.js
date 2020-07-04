ce7fae05c7bb104f71be7dc6eeeacf098ecf0489
Jay <34258275+sell@users.noreply.github.com>
2020-07-03 23:44:09 -0400

    added cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Cors = request("cors")
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const items = require('./routes/api/inputs');
 
 const app = express();
-
-app.use(Cors())
 
 app.use(bodyParser.json());
 
